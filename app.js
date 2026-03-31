8c3d5d62896fab89aa0dc48e5a0b0e9d4ef2248d
ANDRE LUCAS SOUZA RAMOS <232.976682@universidadesenaicimatec.edu.br>
2026-03-31 15:55:25 -0300

    Revisão do app.js

--- app.js
+++ app.js
@@ -5,32 +5,27 @@ const { Pool } = require('pg');
 
 const app = express();
 
-// Configuração do Banco de Dados (Supabase/PostgreSQL)
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
 
-// Middlewares
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// --- ROTAS ---
-
-// 1. Página Principal
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "cadastro.html"));
 });
 
-// 2. Salvar Dados (Chamada pelo Formulário)
 app.post('/api/salvar', async (req, res) => {
     const { nome, celular, email } = req.body;
     const sql = 'INSERT INTO clientes (nome, celular, email) VALUES ($1, $2, $3)';
     
     try {
         await pool.query(sql, [nome, celular, email]);
-        // Redireciona para a home para atualizar a lista automaticamente
         res.redirect('/'); 
     } catch (err) {
         console.error(err);
@@ -38,7 +33,6 @@ app.post('/api/salvar', async (req, res) => {
     }
 });
 
-// 3. API para Listar (Chamada pelo Script na tabela)
 app.get('/api/listar', async (req, res) => {
     try {
         const results = await pool.query('SELECT * FROM clientes ORDER BY id_cliente DESC');
@@ -48,7 +42,6 @@ app.get('/api/listar', async (req, res) => {
     }
 });
 
-// Iniciar Servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`✅ Servidor rodando em: http://localhost:${PORT}`);
